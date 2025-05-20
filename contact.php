ec226dd0d00dadb247b9004fec2241ce57af40d4
AlinaD-2912 <dvornyk.alina2912@gmail.com>
2025-05-20 11:43:30 +0200

    la structure de formulaire

--- contact.php
+++ contact.php
@@ -14,6 +14,30 @@
     <label for="prenom">Preccisez votre prénom</label><br>
     <input type="text" id="prenom" name="prenom"><br>
 
+    <label for="email">Preccisez votre email</label><br>
+    <input type="email" id="email" name="email"><br>
+
+    <fieldset>
+        <legend>Choissisez votre raison de contact</legend>
+
+        <div>
+            <input type="radio" id="serviceInfo" name="raison" />
+            <label for="serviceInfo">Service informatiqque</label>
+        </div>
+
+        <div>
+            <input type="radio" id="hr" name="raison"/>
+            <label for="hr">Human Resources manager</label>
+        </div>
+
+        <div>
+            <input type="radio" id="serviceTech" name="raison" />
+            <label for="serviceTech">Service technique</label>
+        </div>
+    </fieldset>
+
+    <label for="message">Vous pouvez ajouter la commentaire</label><br>
+    <textarea name="message" id="message"></textarea>
     
 
 </form>
